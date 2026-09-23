83889529317a3e367095c6b38dea9b0c79a0f8e9
Andrej Hudačko <andrej.hudacko@gmail.com>
2026-09-23 13:18:24 +0200

    pridal som srandovnu funkciu

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// Definovanie funkcie
+function zrataj(a, b) {
+  return a + b;
+}
+
+// Zavolanie funkcie s číslami 5 a 3
+let vysledok = zrataj(5, 3);
+
+// Výpis výsledku
+console.log("Výsledok je: " + vysledok);
